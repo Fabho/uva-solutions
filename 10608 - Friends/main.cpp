6004f177ae14a036b75f6474d6ff59c2c49c4d74
fabian.calsina@gmail.com <fabian.calsina@gmail.com>
2025-04-08 10:56:33 -0400

    add 10608 - Friends

---
+++ 10608 - Friends/main.cpp
@@ -0,0 +1,86 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef vector<int> vi;
+
+class UnionFind
+{
+private:
+    vi p, rank, setSize;
+    int numSets;
+
+public:
+    UnionFind(int N)
+    {
+        p.assign(N, 0);
+        for (int i = 0; i < N; ++i)
+            p[i] = i;
+        rank.assign(N, 0);
+        setSize.assign(N, 1);
+        numSets = N;
+    }
+
+    int findSet(int i)
+    {
+        return (p[i] == i) ? i : (p[i] = findSet(p[i]));
+    }
+
+    bool isSameSet(int i, int j)
+    {
+        return findSet(i) == findSet(j);
+    }
+
+    int numDisjointSets()
+    {
+        return numSets;
+    }
+
+    int sizeOfSet(int i)
+    {
+        return setSize[findSet(i)];
+    }
+
+    void unionSet(int i, int j)
+    {
+        if (isSameSet(i, j))
+            return;
+        int x = findSet(i), y = findSet(j);
+        if (rank[x] > rank[y])
+            swap(x, y);
+        p[x] = y;
+        if (rank[x] == rank[y])
+            ++rank[y];
+        setSize[y] += setSize[x];
+        --numSets;
+    }
+};
+
+int main()
+{
+    int cases, citizens, joins, citizenOne, citizenTwo, groupSize;
+
+    scanf("%d", &cases);
+
+    while (cases--)
+    {
+        scanf("%d %d", &citizens, &joins);
+
+        UnionFind uf(citizens);
+
+        while (joins--)
+        {
+            scanf("%d %d", &citizenOne, &citizenTwo);
+            uf.unionSet(citizenOne - 1, citizenTwo - 1);
+        }
+
+        groupSize = -1;
+
+        for (int i = 0; i < citizens; i++)
+            groupSize = max(groupSize, uf.sizeOfSet(i));
+
+        printf("%d\n", groupSize);
+    }
+
+    return 0;
+}
