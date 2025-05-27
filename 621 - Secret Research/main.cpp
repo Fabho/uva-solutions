ca00083a92c4aa3235d150aef4bb2c1ce305d31e
fabian.calsina@gmail.com <fabian.calsina@gmail.com>
2025-05-27 11:23:25 -0400

    add 621 - Secret Research

--- 621 - Secret Research/main.cpp
+++ 621 - Secret Research/main.cpp
@@ -15,21 +15,13 @@ int main()
         cin >> input;
 
         if (input == "1" || input == "4" || input == "78")
-        {
             printf("+\n");
-        }
         else if (input.size() > 2 && input[input.size() - 2] == '3' && input[input.size() - 1] == '5')
-        {
             printf("-\n");
-        }
         else if (input.size() > 2 && input[0] == '9' && input[input.size() - 1] == '4')
-        {
             printf("*\n");
-        }
         else if (input.size() > 3 && input[0] == '1' && input[1] == '9' && input[2] == '0')
-        {
             printf("?\n");
-        }
         else
             printf("?\n");
     }
